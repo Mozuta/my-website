0c7bc2f807c059cb3f56ca8af9ac2a8217d01b5b
Qiwei Shan <113519356+Mozuta@users.noreply.github.com>
2024-09-06 14:50:43 +0800

    test git pull

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Hexite',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
